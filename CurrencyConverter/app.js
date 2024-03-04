8d0a425a7ccfb5693de99cfeb3c2774d9cba5163
Harishhgit <138191235+Harishhgit@users.noreply.github.com>
2024-03-04 16:29:58 +0530

    Adding swap functionality between currencies and handling API's Get requests.

--- CurrencyConverter/app.js
+++ CurrencyConverter/app.js
@@ -6,6 +6,7 @@ const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
+const swapbtn = document.querySelector('form i');
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -25,6 +26,15 @@ for (let select of dropdowns) {
   });
 }
 
+swapbtn.addEventListener('click', (evt) =>
+    {
+        exchangeFlag(evt.target);
+     
+        let temp = toCurr.value;
+        toCurr.value = fromCurr.value;
+        fromCurr.value = temp;
+    });
+
 const updateExchangeRate = async () => {
   let amount = document.querySelector(".amount input");
   let amtVal = amount.value;
@@ -32,6 +42,16 @@ const updateExchangeRate = async () => {
     amtVal = 1;
     amount.value = "1";
   }
+  else if(fromCurr.value === toCurr.value)
+    {
+        console.log('same currencies');
+        msg.innerText = `Please select two different currencies !`;
+    }
+  else
+    {
+        msg.innerText = `Something went wrong with API data! try another currency`;
+    }
+  
   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
   let response = await fetch(URL);
   let data = await response.json();
@@ -48,6 +68,12 @@ const updateFlag = (element) => {
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
+
+const exchangeFlag = (element) =>
+{
+    element.parentElement.querySelector('.from img').src = `https://flagsapi.com/${countryList[toCurr.value]}/flat/64.png`;   
+    element.parentElement.querySelector('.to img').src =`https://flagsapi.com/${countryList[fromCurr.value]}/flat/64.png`;
+}
 
 btn.addEventListener("click", (evt) => {
   evt.preventDefault();
